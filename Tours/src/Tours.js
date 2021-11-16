b40fd23cf7787b1882e0f58b89d36f68c45f7123
parpatb9 <47832780+parpatb9@users.noreply.github.com>
2021-11-16 21:53:53 +1300

    Tours project
    useEffect and Conditional Rendering

---
+++ Tours/src/Tours.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Tour from './Tour';
+const Tours = ({tours,removeItem}) => {
+   return <section>
+  <div className="title">
+    <h2>our tour</h2>
+    <div className="underline"></div>
+  </div>
+  <div>
+    {tours.map((tour)=>{
+     
+
+      return <Tour key={tour.id} {...tour} removeItem={removeItem}>
+
+      </Tour>
+    })}
+  </div>
+</section>;
+};
+
+export default Tours;
