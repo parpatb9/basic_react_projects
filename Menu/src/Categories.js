46569e86a21e46bc2234d77f71b83c78edd9d517
parpatb9 <47832780+parpatb9@users.noreply.github.com>
2021-11-16 21:58:51 +1300

    Dynamic Menu using react
    In this project if we add new data and new categories, according to that our app changes buttons and functionalities.

---
+++ Menu/src/Categories.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Categories = ({categories,filterItems}) => {
+  return <div className="btn-container">
+    {
+      categories.map((category,index)=>{
+        return  <button className="filter-btn" onClick={()=>filterItems(category)} key={index}> {category}</button>
+        
+      })
+    }
+   
+    {/* <button className="filter-btn" onClick={()=>filterItems('breakfast')}> Breakfast</button> */}
+    </div>;
+};
+
+export default Categories;
